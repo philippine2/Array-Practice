5a9fa96273abd2916f73e5c48d4ea4289d9293d1
philippine2 <49458817+philippine2@users.noreply.github.com>
2020-09-24 09:41:45 -0700

    Add files via upload

---
+++ ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            ShapeArray();
+            printarray(arr);
+        }
+
+        public static void ShapeArray()
+        {
+            int[] arr = new int[10];
+            arr[0] = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = i;
+               
+            }
+            printarray(arr);
+            Console.ReadLine();
+        }
+
+        public static void printarray(int[] arr)
+        {
+          ;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //arr[i] = i;
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
